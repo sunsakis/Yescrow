9c75f61c8130bc11c784f5043780d6f6a3148d63
Teo <75574673+sunsakis@users.noreply.github.com>
2023-02-09 23:34:57 -0500

    Improved UX

--- pages/index.js
+++ pages/index.js
@@ -124,7 +124,7 @@ export default function Home() {
                 <label>Your e-mail</label><br/>
                 <input 
                   type="email" 
-                  placeholder="your@ddress.com" 
+                  placeholder="@" 
                   onChange={handleEmailChange} 
                   required
                   /><br/>
@@ -146,14 +146,14 @@ export default function Home() {
               <code>{accounts}</code>
               </div>
               <div>
-              <h2 className={styles.description}>How to use the crypto escrow?</h2>
-              <h2>The crypto escrow service commandments</h2>
+              <h2 className={styles.title}>How to use the escrow?</h2><br/><br/><br/>
+              <h2>The 5 commandments:</h2>
               <ul className={styles.card}>
                 <b>!</b> Parties negotiate their terms in private (amount, delivery time).<br/><br/>
-                <b>2@</b> To escrow Ether, buyer provides the seller`s address.<br/><br/>
+                <b>2@</b> Buyer provides the seller`s address when escrowing Ether.<br/><br/>
                 <b>3#</b> Buyer is assigned a unique ID to his/her deposit.<br/><br/>
                 <b>4$</b> Buyer can <Link href="/release">release the escrow</Link> using the ID as a key.<br/><br/>
-                <b>5%</b> In case of disagreement, crow@yescrow.xyz can settle it for 5%.
+                <b>5%</b> In case of disagreement, crow@yescrow.xyz settles it for 5%.
               </ul><br/>
               <h4>Any questions?<br /><br /> Ask the <br />crow@yescrow.xyz <br/>
               <br/> OR <br/><br/> Annoy <br/><br/>the dev<br/> on <br/><br/><a href="https://t.me/sunsakis" rel="nofollow">Telegram</a></h4>           
