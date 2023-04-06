600d0ac3b9a4d33394769c49295da36f2fa2786f
Teo <75574673+sunsakis@users.noreply.github.com>
2023-04-05 21:26:46 -0300

    adapted UI for smaller screens

--- components/header.js
+++ components/header.js
@@ -12,12 +12,12 @@ export default function Header() {
                 </Link>
                 <Link href="/" className="block px-3 py-2 transition hover:text-teal-500"><code>yescrow:</code></Link>
                 <ul className="flex">
-                    <li><Link href="/eth" className="block px-3 py-2 transition hover:text-teal-500">Ethereum</Link></li>
-                    <li><Link href="/nft" className="block px-3 py-2 transition hover:text-teal-500">NFT</Link></li>
-                    <li><Link href="/usdc" className="block px-3 py-2 transition hover:text-teal-500">USDC</Link></li>
-                    <li><Link href="/usdt" className="block px-3 py-2 transition hover:text-teal-500">USDT</Link></li>
-                    <li><Link href="/erc20" className="block px-3 py-2 transition hover:text-teal-500">ERC20</Link></li>
-                    <li><Link href="/btc" className="block px-3 py-2 transition hover:text-teal-500">Bitcoin</Link></li>
+                    <li><Link href="/eth" className="block px-2 py-2 transition hover:text-teal-500">Ethereum</Link></li>
+                    <li><Link href="/nft" className="block px-2 py-2 transition hover:text-teal-500">NFT</Link></li>
+                    <li><Link href="/usdc" className="block px-2 py-2 transition hover:text-teal-500">USDC</Link></li>
+                    <li><Link href="/usdt" className="block px-2 py-2 transition hover:text-teal-500">USDT</Link></li>
+                    <li><Link href="/erc20" className="block px-2 py-2 transition hover:text-teal-500">ERC20</Link></li>
+                    <li><Link href="/btc" className="block px-2 py-2 transition hover:text-teal-500">Bitcoin</Link></li>
                 </ul>
             </div>
             </nav>
