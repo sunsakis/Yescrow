d2788bb38eea5fe56ced82799c5bbb112b953d74
Teo <75574673+sunsakis@users.noreply.github.com>
2023-06-19 21:49:16 +0300

    updated

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,6 @@ import Faq from '../components/faq';
 
 export default function Home() {
 
-  function addMatomo() {
-    var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-      var u="https://yescrow.matomo.cloud/";
-      _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '1']);
-      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.async=true; g.src='//cdn.matomo.cloud/yescrow.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
-  }
-
   function addWebsiteJsonLd() {
     return {
       __html: `{
@@ -102,7 +88,6 @@ export default function Home() {
           type="application/ld+json"
           dangerouslySetInnerHTML={addWebsiteJsonLd()}
           key="website-jsonld"
-          extremelyPrivateMatomo={addMatomo()}
         />
         <title>Yes Crow - a blockchain-native escrow agent. Open source, decentralized, free.</title>
         <meta 
