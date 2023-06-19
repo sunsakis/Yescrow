a95f9ae72948a8ba8358e7f69cac137d5a9430c9
Teo <75574673+sunsakis@users.noreply.github.com>
2023-06-19 21:46:57 +0300

    added Matomo Next

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
+import React, { useEffect } from "react";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { init } from "@socialgouv/matomo-next";
 import { Ethereum, Sepolia } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init({
+      url: process.env.NEXT_PUBLIC_MATOMO_URL,
+      siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID,
+    });
+  }, []);
+
   return (
     <ThirdwebProvider 
       activeChain={Ethereum}
