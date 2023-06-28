99343feb457b24b83bb7f7cbad852d7d456a8145
sunsAkis <75574673+sunsakis@users.noreply.github.com>
2023-06-28 18:59:33 -0300

    Escrow --> Yescrow

--- contract/contracts/Escrow.sol
+++ contract/contracts/Escrow.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Escrow is ReentrancyGuard {
+contract Yescrow is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     /*==============================================================
