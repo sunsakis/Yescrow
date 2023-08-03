745bf0f8b746356f0601ccd59cdac1b95ee4018e
Teo <75574673+sunsakis@users.noreply.github.com>
2023-08-03 14:27:06 +0300

    Update usdt.js

--- pages/usdt.js
+++ pages/usdt.js
@@ -128,7 +128,7 @@ export default function Home() {
   return (
     <div class="container max-w-2xl mx-auto justify-center">
       <Head>
-        <title>USDT Escrow Service: Super Smooth - Yes Crow!</title>
+        <title>USDT Escrow Service - Super Smooth - Yes Crow!</title>
         <meta 
           name="description" 
           content="Get to trust internet strangers without worries. Self-release the payment when you get what you wanted." 
