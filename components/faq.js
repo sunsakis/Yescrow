71b4d76004cd9b0bdceaaa676da21f451a04b030
Teo <75574673+sunsakis@users.noreply.github.com>
2023-08-04 04:09:55 +0300

    Update faq.js

--- components/faq.js
+++ components/faq.js
@@ -232,38 +232,6 @@ export default function Faq() {
                             </div>
                         </div>
             </div>
-            <div class="relative overflow-hidden m-3">
-                    <input 
-                        type="checkbox" 
-                        class="
-                            peer
-                            absolute top-0 inset-x-0
-                            w-full h-12
-                            opacity-0 z-10 cursor-pointer
-                            "
-                    />
-                        <div class="
-                            rounded-xl peer-checked:rounded-b-none
-                            bg-[#161618]
-                            peer-checked:bg-matrix
-                            h-12 w-fill pl-5
-                            flex items-center
-                        ">
-                            <h2 class="text-white">
-                                How can I know that I can trust Yes Crow, though?
-                            </h2>
-                        </div>
-                        <div class="
-                            absolute top-3 right-3
-                            text-red
-                            transition-transform duration-500
-                            rotate-0 peer-checked:rotate-180
-                            ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                            </svg>
-                        </div>
-            </div>
         </section>
     )
 }
