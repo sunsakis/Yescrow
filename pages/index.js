085fcda246886ca25c48e00ac95c8d088a2a871d
Teo <75574673+sunsakis@users.noreply.github.com>
2023-10-25 11:09:38 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ export default function Home( { data } ) {
                       }
                   }
                 ]
-            }
+            }}
               ]
           }      
         `,
