e66b83870ab3c536871256a383f37b731d3f5db5
Teo <75574673+sunsakis@users.noreply.github.com>
2023-11-29 00:31:23 +0200

    removed YESCROW token socials

--- pages/index.js
+++ pages/index.js
@@ -188,7 +188,7 @@ export default function Home( { data } ) {
               <p>Yes Crow does not endorse $CROW in any way.</p>
               <br/> 
             <p>Yes Crow does not believe social media. We believe in decentralization.</p>
-            <p className="m-4 font-medium text-center">
+            {/* <p className="m-4 font-medium text-center">
                 $CROW socials:</p>
               <div className="flex justify-center">
                 <Link href="https://t.me/YesCrowETH" rel="nofollow noopenner external" target="_blank">
@@ -197,7 +197,7 @@ export default function Home( { data } ) {
                 <Link href="https://x.com/Yescrowerc20" rel="nofollow noopenner external" target="_blank">
                   <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" className="fill-white m-3"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
                 </Link>
-              </div>
+              </div> */}
           </div>
         <br/>
         <br/>
